65605b0f633013c694562949b3eab3258d798569
Walker-TW <thomas.w.walker@live.co.uk>
2020-01-07 13:16:50 +0000

    Input forms added

--- app.rb
+++ app.rb
@@ -10,7 +10,14 @@ get '/secret' do
   "Im a secret look at me Ben "
 end
 
-get '/cat' do
+get '/random-cat' do
   @names = ["Amigo", "Oscar", "Viking","Tuxedo",'Flopsy-Mopsy-Cottentail'].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @names = params[:name]
+  @addiction = params[:addiction]
   erb(:index)
 end
